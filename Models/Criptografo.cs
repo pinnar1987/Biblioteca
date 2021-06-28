94be0182771ddc5345e0a0d80b1424f7a0750347
Rafael <pinnar.19@hotmail.com>
2021-06-28 20:48:37 -0300

    Criptografo atualizado

---
+++ Models/Criptografo.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Biblioteca.Models
+{
+  public class Criptografo
+  {
+    public static string TextoCriptografado(string textoClaro)
+    {
+      MD5 MD5Hasher = MD5.Create();
+      byte[] By = Encoding.Default.GetBytes(textoClaro);
+      byte[] byteCriptografado = MD5Hasher.ComputeHash(By);
+      StringBuilder SB = new StringBuilder();
+      foreach (byte b in byteCriptografado)
+      {
+        string DebugB = b.ToString("x2");
+        SB.Append(DebugB);
+      }
+      return SB.ToString();
+    }
+  }
+}
